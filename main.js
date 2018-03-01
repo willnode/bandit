79d9cc7ff2cab4d89c1ec54d75def6a1a0618601
Wildan Mubarok <willnode@wellosoft.net>
2018-03-02 06:19:29 +0700

    javascript on buttons complete

--- main.js
+++ main.js
@@ -5,9 +5,18 @@ var input, cmd, head, data = {
     autoExec: !!localStorage["bandit-autoexec"],
     setCMD: (c) => {
         localStorage["bandit-cmd"] = data.cmd = c;
-        if (localStorage["bandit-autoexec"] = data.autoExec)
-            data.goAll();
+        if (localStorage["bandit-autoexec"] = data.autoExec) {
+            if (input.selectionStart === input.selectionEnd)
+                data.goAll();
+            else
+                data.goSelected();
+
+            // a hacky way to close the menu
+            data.killTheMenu = true;
+            setTimeout(() => data.killTheMenu = false, 300);
+        }
     },
+    killTheMenu: false,
     menu: {
         encode: window.encode,
         decode: window.decode,
@@ -25,10 +34,31 @@ var input, cmd, head, data = {
         data.save();
     },
     goPerLine: () => {
+        var txt2 = [];
+        var cmd2 = `(${data.cmd || '$'})`;
+        data.text.split('\n').forEach(el => {
+            window.$ = el;
+            txt2.push(eval(cmd2));
+        });
+        localStorage["bandit-text"] = data.text = txt2.join('\n');
 
+        // a hacky way to record undo
+        input.focus();
+        input.select();
+        document.execCommand("insertText", false, data.text);
+        input.select();
+        data.save();
     },
     goSelected: () => {
+        var start = input.selectionStart;
+        window.$ = data.text.substring(start, input.selectionEnd);
 
+        // a hacky way to record undo
+        input.focus();
+        document.execCommand("insertText", false, eval(`(${data.cmd || '$'})`));
+        localStorage["bandit-text"] = data.text = input.value;
+        input.selectionStart = start;
+        data.save();
     },
     undo: () => {
         input.focus();
@@ -56,10 +86,10 @@ var app = new Vue({
 input = document.getElementById('text');
 head = document.getElementById('head');
 
-function syncSize () {
-   head.style.width = input.style.width;
-   localStorage["bandit-width"] = input.style.width;
-   localStorage["bandit-height"] = input.style.height;
+function syncSize() {
+    head.style.width = input.style.width;
+    localStorage["bandit-width"] = input.style.width;
+    localStorage["bandit-height"] = input.style.height;
 }
 
 new MutationObserver(syncSize).observe(input, {
