48050b9df15c6331f237deb228a4bbb8547f174f
Wildan Mubarok <willnode@wellosoft.net>
2018-03-02 04:57:32 +0700

    localStorage + head styling + more buttons

--- main.js
+++ main.js
@@ -1,27 +1,50 @@
 
 var input, cmd, head, data = {
-    text: '',
-    cmd: '$',
-    autoExec: false,
+    text: localStorage["bandit-text"] || '',
+    cmd: localStorage["bandit-cmd"] || '$',
+    autoExec: !!localStorage["bandit-autoexec"],
     setCMD: (c) => {
-        data.cmd = c;
-        if (data.autoExec)
-            data.go();
+        localStorage["bandit-cmd"] = data.cmd = c;
+        if (localStorage["bandit-autoexec"] = data.autoExec)
+            data.goAll();
     },
     menu: {
         encode: window.encode,
         decode: window.decode,
         template: window.template,
     },
-    go: () => {
+    goAll: () => {
         window.$ = data.text;
-        data.text = eval(`(${data.cmd || '$'})`);
+        localStorage["bandit-text"] = data.text = eval(`(${data.cmd || '$'})`);
 
         // a hacky way to record undo
         input.focus();
         input.select();
         document.execCommand("insertText", false, data.text);
         input.select();
+        data.save();
+    },
+    goPerLine: () => {
+
+    },
+    goSelected: () => {
+
+    },
+    undo: () => {
+        input.focus();
+        document.execCommand("undo");
+    },
+    copy: () => {
+        input.focus();
+        if (input.selectionStart === input.selectionEnd)
+            input.select();
+        document.execCommand("copy");
+    },
+    save: () => {
+        localStorage["bandit-text"] = data.text;
+    },
+    popout: () => {
+        window.open('/index.html', '_blank');
     }
 }
 
@@ -33,8 +56,16 @@ var app = new Vue({
 input = document.getElementById('text');
 head = document.getElementById('head');
 
-new MutationObserver(() => head.style.width = input.style.width
-).observe(input, {
+function syncSize () {
+   head.style.width = input.style.width;
+   localStorage["bandit-width"] = input.style.width;
+   localStorage["bandit-height"] = input.style.height;
+}
+
+new MutationObserver(syncSize).observe(input, {
     attributes: true,
     attributeFilter: ["style"]
 })
+
+input.style.width = localStorage["bandit-width"] || '500px';
+input.style.height = localStorage["bandit-height"] || '7em';
