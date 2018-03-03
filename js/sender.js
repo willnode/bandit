aeb2fc56231a12804b479565d2397a3eec8bd8cf
Wildan Mubarok <willnode@wellosoft.net>
2018-03-03 19:45:22 +0700

    full xhr page

--- js/sender.js
+++ js/sender.js
@@ -1,42 +1,100 @@
 var httpWords = ['GET', 'POST', 'HEAD', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS']
-function send(word) {
-     word = word.replace(/\r/g, '');
-    var i = 0;
-    // first line is url and the mode
-    var mode = word.match(/^(\w*) /)[1];
-    if (!httpWords.includes(mode)) mode = 'GET';
-    else i += mode.length;
+var input = document.getElementById('text');
+var reqCors = document.getElementById('cors');
+var reqHead = document.getElementById('res-head');
+var reqBody = document.getElementById('res-body');
+var reqPop = document.getElementById('pop');
 
-    var url = word.substr(i).match(/^(.+)(\n[^&\/])?/m)[1];
-    i += url.length;
-    var head = '', body = undefined;
-    url = url.replace(/\n/g, '').trim();
-    if (i < word.length) {
-        var section = word.substr(i).split('\n\n');
-        if (section.length > 0)
-            head = section[0];
-        if (section.length > 1)
-            body = section.slice(1).join('\n\n');
+function parse(word) {
+    word = word.replace(/\r/g, '');
+    var i = 0; var fullbody = false;
+    // first line is url and the mode
+    var mode = (word.match(/^(\w*) /) || [''])[1];
+    if (fullbody = httpWords.includes(mode))
+        i += mode.length;
+    else
+        mode = 'GET';
+
+    var url = '', head = undefined, body = undefined;
+    if (fullbody) {
+        // parse just like usual HTTP request
+        var section = word.substr(i).split('\n', 2);
+        url = section[0].trim();
+        var section2 = (section[1] || '').split('\n\n', 2);
+        head = section2.length > 0 ? section2[0] : undefined;
+        body = section2.length > 1 ? section2[1] : undefined;
+    } else {
+        // URL on first paragraph, HTTP header in second. Separated by a blank line.
+        var section = word.substr(i).split('\n\n', 2);
+        url = section[0].trim().replace(/\n/gm, '');
+        head = section.length > 1 ? section[1] : undefined;
     }
 
-    if (!url) return;
-    var xhr = new XMLHttpRequest();
-    xhr.open(mode, url, true);
+    if (!url) return null;
+
     if (head) {
+        var heads = {};
         head.trim().split('\n').forEach(x => {
             var section = x.match(/^(.+)\:(.*)/);
-            xhr.setRequestHeader(section[1].trim(), section[2].trim());
+            heads[section[1].trim()] = section[2].trim();
         })
+        head = heads;
     }
+
+    return {
+        method: mode,
+        url: url,
+        head: head,
+        body: body,
+    };
+}
+
+function mount(obj, type) {
+    if (window.obj)
+        URL.revokeObjectURL(window.obj);
+    return window.obj = URL.createObjectURL(new Blob(typeof obj === 'string' ? [obj] : obj, { type: type || `*/*` }));
+}
+
+var send = (cors) => {
+    var req = parse(input.value);
+    if (!req) return;
+
+    reqCors.classList.add('hidden');
+    reqPop.classList.add('hidden');
+
+    var xhr = new XMLHttpRequest();
+    xhr.open(req.method, (cors || '') + req.url);
+    for (const head in req.head)
+        xhr.setRequestHeader(head, req.head[head]);
+
     xhr.onreadystatechange = () => {
         if (xhr.readyState === 4) {
-            console.log(xhr.status);
-            console.log(xhr.getAllResponseHeaders());
-            console.log(xhr.responseText);
+            if (xhr.status === 0) {
+                reqCors.classList.remove('hidden');
+                reqPop.classList.add('hidden');
+            } else
+                reqPop.classList.remove('hidden');
+            reqHead.innerText = `${xhr.status} ${xhr.statusText}\n${xhr.getAllResponseHeaders()}`;
+            var type = xhr.getResponseHeader('content-type') || '*/*';
+            mount(xhr.response, type);
+            reqBody.innerText = xhr.response;
         }
     }
-    xhr.send(body);
+    xhr.send(req.body);
 }
-document.getElementById('submit').onclick = () => {
-  send( document.getElementById('input').value);
+
+document.getElementById('xhr').onclick = () => send();
+
+reqCors.onclick = () => {
+    var proxy = localStorage['cors'] || '';
+    if (!proxy)
+        proxy = prompt("Enter CORS proxy URL\nYou can edit this later via localStorage", "");
+    if (!proxy) return;
+
+    send(localStorage['cors'] = proxy.slice(-1) === '/' ? proxy : proxy + '/');
+
+}
+
+reqPop.onclick = () => {
+    window.open(obj, '_blank');
 }
